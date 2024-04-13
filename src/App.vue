3296aeb9286ed8da33ec20d8277db48a393317e8
Manuel Maisano <manumaisano@gmail.com>
2024-04-13 17:41:58 +0200

    add component and headercss

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
     <HeaderComponent />
+    <MainComponent />
+    <FooterOne />
+
 </template>
 
 <script>
-import HeaderComponent from './components/HeaderComponent.vue'
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterOne from './components/FooterOne.vue';
     export default {
         name:'App',
         components:{
-            HeaderComponent
+            HeaderComponent,
+            MainComponent,
+            FooterOne
         },
         data(){
             return{
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,21 +1,72 @@
 <template>
     <header>
-        <img src="../assets/img/dc-logo.png" alt="Logo">
+        <div id="header">
+            <img src="../assets/img/dc-logo.png" alt="Logo">
+
+
+            <ul>
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBILES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </div>
     </header>
 </template>
 
 <script>
-    export default {
-        name: 'HeaderComponent',
-        data(){
-            return{
-                title: 'HeaderComponent'
-            }
+export default {
+    name: 'HeaderComponent',
+    data() {
+        return {
+            title: 'HeaderComponent'
         }
-        
     }
+
+}
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
 
+
+
+
+
+header {
+    width: 1200px;
+    display: flex;
+    align-items: center;
+    margin: auto;
+    
+}
+
+#header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+ul {
+    display: flex;
+    align-items: center;
+    margin: auto;
+    padding-left: 250px;
+}
+
+li {
+    list-style: none;
+    margin: 10px;
+}
+
+a {
+    text-decoration: none;
+    color: black;
+    font-weight: bold;
+}
 </style>
