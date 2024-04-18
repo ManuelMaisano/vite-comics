56f144633f1aca3ede295f4c005d9f7ec1363c4a
Manuel Maisano <manumaisano@gmail.com>
2024-04-18 16:53:22 +0200

    cardcomponent import in maincomponent

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,23 +1,34 @@
 <template>
-
     <main>
-        <div class="container">
-          <h1><--CONTENT--></h1>
-
+        <div class="container text-center">
+            <h2 class="text-white  ">CURRENT SERIES</h2>
+            <div class="row">
+                <div class="col-12 col-md-3 col-lg-2" v-for="(item, index) in comics" :key="index">
+                    <CardComponent :image="item.thumb" :series="item.series" :price="item.price" />
+                </div>
+            </div>
         </div>
-
     </main>
-
 </template>
 
 <script>
+import CardComponent from './MainComponent/CardComponent.vue';
+import { comics } from '../data/store';
 export default {
     name: 'MainComponent',
-    data() {
-        return {
-
+    components :{
+        CardComponent
+    },
+  
+    data(){
+        return{
+            comics
         }
-    }
+    },
+    mounted() {
+        console.log(comics)
+       
+    },
 
 }
 </script>
@@ -25,11 +36,14 @@ export default {
 <style lang="scss" scoped>
 main {
     background-color: #1C1C1C;
-    height: 100px;
-}
-h1{
-    padding: 20px;
-    color: white;
    
 }
+h2{
+            background-color: #0282F9;
+            padding: 10px 0px;
+            width: 300px;
+            margin-right: auto;
+            transform: translateY(-50%);
+           
+        }
 </style>
---
+++ src/components/MainComponent/CardComponent.vue
@@ -0,0 +1,31 @@
+<template>
+      <div>
+        <div>
+            <img :src="image" class="img-fluid w-100 " :alt="series">
+        </div>
+        <div class="card-body text-start  my-2">
+            <h5 class="text-white">{{ series }}</h5>
+        </div>
+    </div>
+    <div class="d-flex align-items-end justify-content-between">
+        <span class="text-white ">{{ price }}</span>
+      
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'CardComponent',
+        props: [
+            'image',
+            'series',
+            'price',
+            'link',
+        ],
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
